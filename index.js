ded73ed5c02f6ee20ce7d0338df938472db08e6a
hassanuahmad <ahmad12p@uwindsor.ca>
2021-11-19 22:35:31 -0500

    messeges are getting sent to the group chat

--- index.js
+++ index.js
@@ -3,32 +3,35 @@ const { Telegraf } = require("telegraf");
 
 const bot = new Telegraf(process.env.TOKEN);
 
-bot.use("message", (ctx) => {
-    const date = new Date();
-    let day = date.getDay();
-    let time =
-        date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+const date = new Date();
+const day = date.getDay();
+const time = date.getTime();
+const trigger = date.setHours(22, 6, 11);
 
-    if (day === 1 || day === 3) {
-        ctx.telegram.sendMessage(
-            process.env.CHAT_ID,
-            "Subject Today: \n7:10 - 8:10 -> Fiqh"
-        );
-    } else if (
-        day === 2 ||
-        (day === 5 &&
-            date.getTime() === new Date().setHours(21, 40, 15).getTime())
-    ) {
-        ctx.telegram.sendMessage(
-            process.env.CHAT_ID,
-            "Subjects Today: \n6:00 - 7:00 -> Hadith\n7:00 - 8:00 -> Tafseer\n8:00 - 8:30 -> T.Q."
-        );
-    } else if (day === 4) {
-        ctx.telegram.sendMessage(
-            process.env.CHAT_ID,
-            "Subjects Today: \n6:00 - 7:00 -> An-Nahw\n7:00 - 8:00 -> Qasas\n8:00 - 8:30 -> Arabic Speaking"
-        );
-    }
-});
+if (day === 1 || day === 3) {
+    bot.telegram.sendMessage(
+        process.env.CHAT_ID,
+        "Subject Today: \n7:10 - 8:10 -> *Fiqh*",
+        {
+            parse_mode: "MarkdownV2",
+        }
+    );
+} else if (day === 2 || (day === 5 && time >= trigger)) {
+    bot.telegram.sendMessage(
+        process.env.CHAT_ID,
+        "Subjects Today: \n6:00 : 7:00 : *Hadith*\n7:00 : 8:00 : *Tafseer*\n8:00 : 8:30 : *TQ*",
+        {
+            parse_mode: "MarkdownV2",
+        }
+    );
+} else if (day === 4) {
+    bot.telegram.sendMessage(
+        process.env.CHAT_ID,
+        "Subjects Today: \n6:00 - 7:00 -> *An-Nahw*\n7:00 - 8:00 -> *Qasas*\n8:00 - 8:30 -> *Arabic Speaking*",
+        {
+            parse_mode: "MarkdownV2",
+        }
+    );
+}
 
 bot.launch();
