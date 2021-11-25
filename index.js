f7e21dce75aa95b48fcf6f4fc75c11c291d5ec17
hassanuahmad <ahmad12p@uwindsor.ca>
2021-11-24 23:20:00 -0500

    added node-cron for the scheduled messege

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { Telegraf } = require("telegraf");
+const cron = require("node-cron");
 
 const API_TOKEN = process.env.API_TOKEN || "";
 const PORT = process.env.PORT || 3000;
@@ -11,8 +12,6 @@ bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
 
 const date = new Date();
 const day = date.getDay();
-const time = date.getTime();
-const trigger = date.setHours(22, 49, 10);
 
 const allDayNames = new Array(7);
 allDayNames[0] = "Sunday";
@@ -25,36 +24,39 @@ allDayNames[6] = "Saturday";
 
 let dayName = allDayNames[date.getDay()];
 
-if (day === 1 || (day === 3 && time >= trigger)) {
-    bot.telegram.sendMessage(
-        process.env.CHAT_ID,
-        `${dayName}'s Subject: \n\n7:10 : 8:10 : *Fiqh*`,
-        {
-            parse_mode: "MarkdownV2",
+cron.schedule(
+    "23 23 * * *",
+    () => {
+        if (day === 1 || day === 3) {
+            bot.telegram.sendMessage(
+                process.env.CHAT_ID,
+                `${dayName}'s Subject: \n\n7:10 : 8:10 : *Fiqh*`,
+                {
+                    parse_mode: "MarkdownV2",
+                }
+            );
+        } else if (day === 2 || day === 5) {
+            bot.telegram.sendMessage(
+                process.env.CHAT_ID,
+                `${dayName}'s Subjects: \n\n6:00 : 7:00 : *Hadith*\n7:00 : 8:00 : *Tafseer*\n8:00 : 8:30 : *TQ*`,
+                {
+                    parse_mode: "MarkdownV2",
+                }
+            );
+        } else if (day === 4) {
+            bot.telegram.sendMessage(
+                process.env.CHAT_ID,
+                `${dayName}'s Subjects: \n\n6:00 : 7:00 : *An-Nahw*\n7:00 : 8:00 : *Qasas*\n8:00 : 8:30 : *Arabic Speaking*`,
+                {
+                    parse_mode: "MarkdownV2",
+                }
+            );
         }
-    );
-} else if (day === 2 || (day === 5 && time >= trigger)) {
-    bot.telegram.sendMessage(
-        process.env.CHAT_ID,
-        `${dayName}'s Subjects: \n\n6:00 : 7:00 : *Hadith*\n7:00 : 8:00 : *Tafseer*\n8:00 : 8:30 : *TQ*`,
-        {
-            parse_mode: "MarkdownV2",
-        }
-    );
-} else if (day === 4 && time >= trigger) {
-    bot.telegram.sendMessage(
-        process.env.CHAT_ID,
-        `${dayName}'s Subjects: \n\n6:00 : 7:00 : *An-Nahw*\n7:00 : 8:00 : *Qasas*\n8:00 : 8:30 : *Arabic Speaking*`,
-        {
-            parse_mode: "MarkdownV2",
-        }
-    );
-}
+    },
+    {
+        scheduled: true,
+        timezone: "America/Toronto",
+    }
+);
 
 bot.launch();
-
-/*
-git add .
-git commit -m 'commit message'
-git push heroku master
-*/
