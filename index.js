66d16fe8811df3a9717609f29488b3ec21bbda7c
hassanuahmad <ahmad12p@uwindsor.ca>
2021-11-24 23:44:01 -0500

    added error handling for sendMessage

--- index.js
+++ index.js
@@ -28,29 +28,41 @@ cron.schedule(
     "23 23 * * *",
     () => {
         if (day === 1 || day === 3) {
-            bot.telegram.sendMessage(
-                process.env.CHAT_ID,
-                `${dayName}'s Subject: \n\n7:10 : 8:10 : *Fiqh*`,
-                {
-                    parse_mode: "MarkdownV2",
-                }
-            );
+            bot.telegram
+                .sendMessage(
+                    process.env.CHAT_ID,
+                    `${dayName}'s Subject: \n\n7:10 : 8:10 : *Fiqh*`,
+                    {
+                        parse_mode: "MarkdownV2",
+                    }
+                )
+                .catch(function (err) {
+                    console.log(err);
+                });
         } else if (day === 2 || day === 5) {
-            bot.telegram.sendMessage(
-                process.env.CHAT_ID,
-                `${dayName}'s Subjects: \n\n6:00 : 7:00 : *Hadith*\n7:00 : 8:00 : *Tafseer*\n8:00 : 8:30 : *TQ*`,
-                {
-                    parse_mode: "MarkdownV2",
-                }
-            );
+            bot.telegram
+                .sendMessage(
+                    process.env.CHAT_ID,
+                    `${dayName}'s Subjects: \n\n6:00 : 7:00 : *Hadith*\n7:00 : 8:00 : *Tafseer*\n8:00 : 8:30 : *TQ*`,
+                    {
+                        parse_mode: "MarkdownV2",
+                    }
+                )
+                .catch(function (err) {
+                    console.log(err);
+                });
         } else if (day === 4) {
-            bot.telegram.sendMessage(
-                process.env.CHAT_ID,
-                `${dayName}'s Subjects: \n\n6:00 : 7:00 : *An-Nahw*\n7:00 : 8:00 : *Qasas*\n8:00 : 8:30 : *Arabic Speaking*`,
-                {
-                    parse_mode: "MarkdownV2",
-                }
-            );
+            bot.telegram
+                .sendMessage(
+                    process.env.CHAT_ID,
+                    `${dayName}'s Subjects: \n\n6:00 : 7:00 : *An-Nahw*\n7:00 : 8:00 : *Qasas*\n8:00 : 8:30 : *Arabic Speaking*`,
+                    {
+                        parse_mode: "MarkdownV2",
+                    }
+                )
+                .catch(function (err) {
+                    console.log(err);
+                });
         }
     },
     {
